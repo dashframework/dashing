5b7bd51e5f5487e7ee8d2177b3bafbbb35213da7
Jared Rauh <jared.rauh@samaritanministrires.org>
2016-10-19 18:19:42 -0500

    show/hide code in example page

--- example/scripts/scripts.js
+++ example/scripts/scripts.js
@@ -1,5 +1,3 @@
-function toggle(toggleThis, obj) {
-    var $input = $(obj);
-    if ($input.prop('checked')) $(toggleThis).hide();
-    else $(toggleThis).show();
-}
+$("#switch--checkbox").click(function () {
+  $(".code-toggle").toggle()
+});
