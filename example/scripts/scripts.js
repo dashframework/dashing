59870d193cc35090f689f055b375ad19d8e4994f
Jared Rauh <jared.rauh@samaritanministrires.org>
2016-11-02 20:49:41 -0500

    Restructured example templates

--- example/scripts/scripts.js
+++ example/scripts/scripts.js
@@ -1,5 +1,24 @@
-function toggle(toggleThis, obj) {
-    var $input = $(obj);
-    if ($input.prop('checked')) $(toggleThis).hide();
-    else $(toggleThis).show();
-}
+// $(function() {
+//   $("#sidebarNavigation").load("/example/templates/sidebar.html", function() {
+//     var url = window.location.href;
+//     var findNameRegex = /[\w-]+\./;
+//     var fileName = findNameRegex.exec(url)[0];
+//     fileName = fileName.slice(0, -1);
+//
+//     var className = '#sidebarNavigation #'+fileName;
+//
+//     $(className).addClass('active');
+//   });
+// });
+
+$(function() {
+  $("#sidebarNavigation").load("/example/templates/sidebar.html", function() {
+    var url = window.location.href;
+    var findNameRegex = /(\w+)(\/\w+)[\w-]+\./;
+    var urlString = findNameRegex.exec(url)[0];
+    var cutoff = urlString.indexOf('/');
+    var fileName = urlString.substring(0, cutoff);
+    var className = '#sidebarNavigation #'+fileName;
+    $(className).addClass('active');
+  });
+});
