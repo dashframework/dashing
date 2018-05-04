d6910307e6a3d19d32898bbb1a83c1fea83b2af0
Cameron Perera <cameron.perera@samaritanministries.org>
2018-05-04 10:37:59 -0500

    rendering view working, needs styling

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+let webpack = require("webpack");
+let path = require("path");
+
+const PROJECT_ROOT = path.resolve(__dirname);
+
+module.exports = {
+    devtool: "source-map",
+    entry: [
+        "./scripts/load_bower_components.js",
+        "./scripts/main.js"
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.join(PROJECT_ROOT, "example"),
+                    path.join(PROJECT_ROOT, "scripts")
+                ],
+                loader: "babel-loader"
+            },
+            {
+                test: /\.ejs$/,
+                include: [
+                    path.join(PROJECT_ROOT, "example"),
+                    path.join(PROJECT_ROOT, "scripts")
+                ],
+                loader: "ejs-compiled"
+            }
+        ]
+    },
+    output: {
+        filename: "application.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            "underscore": "underscore",
+            _: "underscore",
+            "jquery": "jquery"
+        }),
+    ],
+    resolve: {
+        alias: {
+            "scripts": path.join(PROJECT_ROOT, "scripts"),
+            "example": path.join(PROJECT_ROOT, "example")
+        },
+        modulesDirectories: [
+            path.join(PROJECT_ROOT, "node_modules"),
+            path.join(PROJECT_ROOT, "bower_components")
+        ]
+    }
+}
